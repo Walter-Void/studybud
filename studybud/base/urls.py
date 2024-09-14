90e58eabeaa31933cd27ed433a36da303cf4ee9c
Walter-Void <wleiva@charlotte.edu>
2024-09-14 18:03:51 -0400

    Add files via upload

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'), #Path to home 
-    path('room/', views.room, name = "room"), #Path to room
+    path('', views.home, name = "home"), #Path to home 
+    path('room/<str:pk>/', views.room, name = "room"), #Path to room
 ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -3,10 +3,22 @@ from django.http import HttpResponse # imports to HttpsResponse to work
 
 # Create your views here.
 
+#List for rooms
+rooms = [
+    {'id': 1, 'name' : 'Lets learn python!'}, #Pretty much discord rooms
+    {'id': 2, 'name' : 'Design with me'},
+    {'id': 3, 'name' : 'Frontend developers'},
+]
 #Function that shows user Home Page instead of the default page
 def home(request):
-    return render(request, 'home.html') #Goes into template folder to go to the home html
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context) #Goes into template folder to go to the home html
 
 #Function that show/sends you to a room
-def room(request):
-    return render(request, 'room.html') #Goes into template folder to go to the room html
+def room(request, pk):
+    room = None
+    for i in rooms: #for loop 
+        if i['id'] == int(pk):
+            room = i 
+    context = {'room' : room}
+    return render(request, 'base/room.html', context) #Goes into template folder to go to the room html
