7ed2523833a2c13136f00624f839693534caa381
Walter-Void <wleiva@charlotte.edu>
2024-09-14 21:22:15 -0400

    Add files via upload

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User #Users from Django 
 # Create your models here.
+
+#Class for Topic
+class Topic(models.Model):
+    name = models.CharField(max_length = 200)
+
+    def __str__(self): return self.name
+    
+#Room Model
+class Room(models.Model):
+    host =  models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
+    topic = models.ForeignKey(Topic, on_delete = models.SET_NULL, null = True)
+    name = models.CharField(max_length=200) #Allows different names
+    description = models.TextField(null = True, blank = True) #Blank page for decription 
+    #participants = 
+    updated = models.DateTimeField(auto_now = True) # Shows time 
+    created = models.DateTimeField(auto_now_add = True) #shows time of creation  
+
+    def __str__(self):
+        return self.name
+    
+#Models that outputs a message 
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE) #A for Users relationship with Room class that deletes all the childern
+    room = models.ForeignKey(Room, on_delete = models.CASCADE) #A for room relationship with Room class that deletes all the childern
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now = True) # Shows time 
+    created = models.DateTimeField(auto_now_add = True) #shows time of creation  
+
+    def __str__(self):
+        return self.body[0:50]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse # imports to HttpsResponse to work
-
+from .models import Room #Imports Room from the models.py
 # Create your views here.
 
 #List for rooms
-rooms = [
-    {'id': 1, 'name' : 'Lets learn python!'}, #Pretty much discord rooms
-    {'id': 2, 'name' : 'Design with me'},
-    {'id': 3, 'name' : 'Frontend developers'},
-]
+#rooms = [
+#    {'id': 1, 'name' : 'Lets learn python!'}, #Pretty much discord rooms
+#    {'id': 2, 'name' : 'Design with me'},
+#    {'id': 3, 'name' : 'Frontend developers'},
+#]
+
 #Function that shows user Home Page instead of the default page
 def home(request):
+    rooms = Room.objects.all() #overrides the other room list 
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context) #Goes into template folder to go to the home html
 
 #Function that show/sends you to a room
 def room(request, pk):
-    room = None
-    for i in rooms: #for loop 
-        if i['id'] == int(pk):
-            room = i 
+    room = Room.objects.get(id =pk)
     context = {'room' : room}
     return render(request, 'base/room.html', context) #Goes into template folder to go to the room html
